c2ff2c906608772e35d78e7d52f1230330b59866
Marcio Leite <contato@marcioleitedev.com.br>
2023-03-23 19:43:31 -0300

    calculadora em javascript

---
+++ Seção4/78.calculadora_funcao_construtora/assets/js/index.js
@@ -0,0 +1,55 @@
+function Calculadora(){
+
+    this.display = document.querySelector('.display')
+
+    this.inicia = () => {
+        this.capturaCliques();
+        this.capturaEnter();
+    }    
+
+    this.capturaEnter = () => {
+        this.display.addEventListener('keypress' , e => {
+            if (e.keyCode !== 13 ) return
+            this.realizaConta()
+        })
+    }
+
+    this.capturaCliques = () => {
+        document.addEventListener('click' , event => {
+            const el = event.target;     
+            if(el.classList.contains('btn-num')) this.addNumDisplay(el)
+            if(el.classList.contains('btn-clear')) this.clear()
+            if(el.classList.contains('btn-del')) this.del(el)
+            if(el.classList.contains('btn-eq')) this.realizaConta(el)
+        });
+    }
+
+    this.realizaConta = () => {
+        try{
+            const conta = eval(this.display.value) 
+
+            if(!conta) {
+                alert('conta invalida')
+                return
+            }
+
+            this.display.value = conta
+        } catch(e){
+            alert('conta inválida')
+            return
+        }
+    }
+
+    this.addNumDisplay = el => {
+        this.display.value += el.innerText;
+        this.display.focus()
+    }  
+    this.clear = el => this.display.value = ""
+    this.del = () => this.display.value = this.display.value.slice(0, -1)
+       
+
+
+}
+
+const calculadora = new Calculadora();
+calculadora.inicia();
