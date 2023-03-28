92f907103223696c13ce39734812cc07e4f82ef5
Marcio Leite <marcio.leite@capitalconsig.local>
2023-03-28 18:52:51 -0300

    arrays

---
+++ Seção5 - Arrays/81.arrays_revisao/index.js
@@ -0,0 +1,34 @@
+
+
+//const nomes = Array('eduardo' , 'Maria' , 'Joana')
+
+const nomes = ['Eduardo' , 'Maria', 'Joana' , 'Rosana' , 'Walda']
+
+const novo = nomes.slice(1,4)
+
+const removido = nomes.shift() // remove do inicio
+
+nomes.push('Eu mesmo') // insere no ultimo array
+nomes.unshift('Marcio') // insere no inicio 
+
+//nomes[2] = 'João'
+
+//novo = [...nomes]
+
+//novo.pop() // remove ultimo elemento
+
+//novo.shift();
+
+//delete nomes[2]
+
+console.log(novo )
+
+const nomeString = "Marcio Barbosa Leite "
+
+const nomess = nomeString.split(" ")
+
+console.log(nomess)
+
+unidos = nomess.join(' ');
+
+console.log(unidos)
