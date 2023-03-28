aa299b9f43a76a20c9bcb3db057f08fdc48e90b5
Marcio Leite <marcio.leite@capitalconsig.local>
2023-03-28 19:15:00 -0300

    splice

---
+++ Seção5 - Arrays/82.splice/index.js
@@ -0,0 +1,20 @@
+const nomes = ['Maria' , 'João' , 'Eduardo' , 'Gabriel' , 'Julia']
+//nomes.splice(indice. delete. elem1, elem2, elem3)
+
+//const removidos = nomes.splice(3, 1 , 'Disbiriflix' , 'Fulano Brow')
+//console.log(nomes , removidos)
+
+//shitf
+//const removidos = nomes.splice(0, 1)
+
+//pop
+//const removidos = nomes.splice(-1, 1)
+
+// push
+//const removidos = nomes.splice(nomes.length , 0 , 'Luiz' , 'Otavio')
+
+//unshift
+const removidos = nomes.splice(0 , 0 , 'Marcio' , 'Leite')
+
+
+console.log(nomes )
