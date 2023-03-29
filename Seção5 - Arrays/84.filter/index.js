d788449704a17d6ff95f9f52862a86d7b7018521
Marcio Leite <marcio.leite@capitalconsig.local>
2023-03-29 19:16:25 -0300

    filter

---
+++ Seção5 - Arrays/84.filter/index.js
@@ -0,0 +1,37 @@
+// FILTER , MAP, REDUCE
+
+const numeros = [5,50, 80, 1,2,3,5,8,7,11,15,22,27]
+
+function callbackFilter(valor , indice, array ){
+    return valor > 10
+
+}
+
+//const numerosFiltrados = numeros.filter(callbackFilter);
+
+//const numerosFiltrados = numeros.filter(function(valor){
+//    return valor > 10
+//})
+
+//const numerosFiltrados = numeros.filter((valor) => {
+ //   return valor > 10
+//})
+
+const numerosFiltrados = numeros.filter(valor => valor > 10)
+
+console.log(numerosFiltrados)
+
+const pessoas = [
+    { nome: 'Luiz' , idade: 65},
+    {nome: 'Maria' , idade: 23},
+    {nome: 'eduardo' , idade: 55},
+    {nome: 'Leticia', idade: 19},
+    {nome: 'Rosana', idade: 32},
+    {nome: 'Wallace' , idade: 47}
+];
+
+const pessoasComNomeGrande = pessoas.filter(function(obj){
+    return obj.nome.length <= 5
+   
+})
+console.log(pessoasComNomeGrande)
